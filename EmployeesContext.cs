b7587766b4f01090914a9c5c524def3fee31a268
amakhyon <a.m.makhyon@gmail.com>
2019-07-15 14:01:43 +0400

    initial commit

---
+++ EmployeesContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Employee_analytics.Models;
+
+
+
+
+namespace Employee_analytics.DAL
+{
+    public class EmployeeContext : DbContext
+    {
+
+        public EmployeeContext() : base("Employee_analytics")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
